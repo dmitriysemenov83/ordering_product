9847a8f24560ce83fe3e41c720fccb83a8e99ca4
Dmitriy <xs_ds@inbox.ru>
2024-11-07 16:42:30 +0500

    change db settings

--- config/settings.py
+++ config/settings.py
@@ -92,9 +92,9 @@ DATABASES = {
         'ENGINE': os.getenv('DB_ENGINE'),
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        # 'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        # 'HOST': os.getenv('DB_HOST'),
+        # 'PORT': os.getenv('DB_PORT'),
     }
 }
 
